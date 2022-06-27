1637df61c5661ad3ff311138005f2047873bc624
Syed Wajid Dev <53843406+syedwajid01@users.noreply.github.com>
2022-06-27 13:16:29 +0530

    Time: 101 ms (65.77%), Space: 14.7 MB (83.73%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        '''
+        
+        input: string n 
+        
+        if n is single digit or <=9
+        example n=8
+        we will have to add 8 1's to get 8 
+        so for single sigit our answer will be 8 
+        
+        if number > 9 or multi digit 
+        
+        example: 
+        32
+        then we will add
+        11
+        11
+        10
+        
+    
+        example: 
+        82734
+        
+        11111
+        11111
+        10111
+        10101
+        10100
+        10100
+        10100
+        10000
+        
+        as you can see for for multiple digits we need to do the same i.e have as many ones as that digit
+        then group together
+        
+        so our answer will be the max digit in the given string (greedy approach)
+        
+
+        '''
+        
+        return int(max(n))
+        
