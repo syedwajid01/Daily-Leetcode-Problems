4ee6d3b4c50f9fbfc89e43001289bc9f1fac9e89
Syed Wajid Dev <53843406+syedwajid01@users.noreply.github.com>
2022-06-28 11:16:08 +0530

    Time: 245 ms (53.37%), Space: 14.7 MB (93.89%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,66 +1,19 @@
-from heapq import *
 class Solution:
     def minDeletions(self, s: str) -> int:
-        '''
+        charFreqMap=Counter(s)
         
-        a:2
-        b:1 
-        
-        min number of string we need to remove 
-        
-        3 3 2 2 1 1
-        
-        do minimum number of deleteion to make all the numbers unique 
-        
-        3:2
-        2:2
-        1:1
-         
-        a:3
-        b:3
-        c:2
-        
-        3:1
-        
-        2:1
-        
-        1:1
-        
-        
-        all are unique 
-        
-        '''
-        charFreq=Counter(s)
-        freqMap={}
-        
-        for _,freq in charFreq.items():
-            freqMap[freq]=freqMap.get(freq,0)+1
-        
-        print(freqMap)
-        
-        heap=[]
-        
-        for freq,count in freqMap.items():
-            if count>1:
-                heappush(heap,[freq,count])
-        
+        seen_freq=set()
         ans=0
-        while heap:
-            freq,count=heappop(heap)
-            
-            while count!=1:
-                count-=1
-                temp=freq
-                while temp!=0:
-                    temp-=1
-                    ans+=1
-                    if temp not in freqMap:
-                        freqMap[temp]=1
+        for char,freq in charFreqMap.items():
+            if freq in seen_freq:
+                while freq!=0:
+                    freq-=1
+                    if freq not in seen_freq:
+                        seen_freq.add(freq)
                         break
-            freqMap[freq]=1
-        print(freqMap)
+                ans+=charFreqMap[char]-freq
+            else:
+                seen_freq.add(freq)
+        
         return ans
-            
-        
-        
         
