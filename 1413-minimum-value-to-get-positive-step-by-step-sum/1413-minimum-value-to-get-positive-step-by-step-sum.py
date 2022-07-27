4e4dd1b6528c8a0c399169c06e3240508af887ef
Syed Wajid Dev <53843406+syedwajid01@users.noreply.github.com>
2022-07-27 11:00:37 +0530

    Time: 51 ms (48.24%), Space: 13.8 MB (55.53%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,35 +1,14 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        right=0
-        for num in nums:
-            if num<0:
-                right+=(-1*num)
-              
-        right+=1
-        left=0
+        minCumSum=nums[0]
+        currSum=nums[0]
+        for i in range(1,len(nums)):
+            currSum+=nums[i]
+            minCumSum=min(minCumSum,currSum)
         
-        
-        def isfeasible(start):
-            curr=start
-            for num in nums:
-                curr+=num
-                if curr<1:
-                    return False
-            return True
-        
-        while left<right:
-            mid=(left+right)//2
-            
-            curr=mid
-            if isfeasible(mid):
-                right=mid
-            else:
-                left=mid+1
-        
-        if left==0:
+        if minCumSum>=1:
             return 1
-        return left
-            
-            
+        
+        return 1-minCumSum
             
         
