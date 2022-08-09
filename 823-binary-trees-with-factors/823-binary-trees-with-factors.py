6e329f356299cf8229a77d6c6179b642ab74793d
Syed Wajid Dev <53843406+syedwajid01@users.noreply.github.com>
2022-08-09 12:35:24 +0530

    Time: 696 ms (49.49%), Space: 14.1 MB (90.91%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        
+        #dp 
+        n=len(arr)
+        dp=[1]*n
+        arr.sort()
+        
+        index={}
+        for i in range(len(arr)):
+            index[arr[i]]=i
+            
+        
+        
+        for i in range(n):
+            for j in range(i):
+                if arr[i]%arr[j]==0:
+                    #then j is left child
+                    right=arr[i]//arr[j]
+                    
+                    if right in index:
+                        dp[i]=dp[i]+(dp[j]*dp[index[right]])
+                
+        
+        print(dp)
+        return sum(dp)%(10**9+7)
+                        
+        
+        
